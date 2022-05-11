c6c85697d6772a2cf33faa916ebefcc39321f51d
asultana181 <101515265+asultana181@users.noreply.github.com>
2022-05-11 20:07:52 +0600

    Update simple.java
    Factory Pattern Added

--- simple.java
+++ simple.java
@@ -35,14 +35,48 @@ class Rectangle implements Shape
 	 }
 }
 
+public class ShapeFactory {
+   public Shape getShape(String shapeType){
+      if(shapeType == null){
+         return null;
+      }		
+      if(shapeType.equalsIgnoreCase("CIRCLE")){
+         return new Circle();
+         
+      } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+         return new Rectangle();
+         
+      } else if(shapeType.equalsIgnoreCase("SQUARE")){
+         return new Square();
+      }
+      
+      return null;
+   }
+}
+
 class Painter
 {
 	 public static void main(String[] args) 
 	 {
 		 
 		 
-		 Circle c=new Circle();
-		 c.draw();
+		//get an object of Circle and call its draw method.
+	      Shape shape1 = shapeFactory.getShape("CIRCLE");
+
+	      //call draw method of Circle
+	      shape1.draw();
+
+	      //get an object of Rectangle and call its draw method.
+	      Shape shape2 = shapeFactory.getShape("RECTANGLE");
+
+	      //call draw method of Rectangle
+	      shape2.draw();
+
+	      //get an object of Square and call its draw method.
+	      Shape shape3 = shapeFactory.getShape("SQUARE");
+
+	      //call draw method of square
+	      shape3.draw();
 		
 	 }
 }
